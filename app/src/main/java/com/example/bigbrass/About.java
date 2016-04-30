a6135c9da63955e03e11928f949d5446a1a84028
Donrwalsh <donrwalsh@gmail.com>
2016-04-30 05:32:40 -0500

    Title screen gone! Homepage -> About toggle via buttons

--- app/src/main/java/com/example/bigbrass/About.java
+++ app/src/main/java/com/example/bigbrass/About.java
@@ -1,28 +1,24 @@
 package com.example.bigbrass;
 
 import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
 import com.example.bigbrass.chunky.R;
 
-public class MainActivity extends AppCompatActivity {
+public class About extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //Remove notification bar
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_about);
     }
-
-    public void openAbout(View view)
+    public void returnHome(View view)
     {
-        Intent intent = new Intent(this, About.class);
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
-
-
 }
